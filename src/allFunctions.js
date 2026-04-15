0a33e0b54a68ee0822186c343d8d397088c24740
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-16 02:33:29 +0800

    Added the js script to house all the functions to test

---
+++ src/allFunctions.js
@@ -0,0 +1,9 @@
+/* "allFunctions.js" for "testing_practice_jest_TOP" */
+
+const myFunctions = {
+  capitalize: (word) => {
+    console.log("capitalize the first letter");
+  },
+};
+
+export { myFunctions };
