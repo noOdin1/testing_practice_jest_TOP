35068f4116d9f24be424b422784c538aae8563f0
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-16 03:17:28 +0800

    Calculator object with add, sub, mul and div functions

--- src/allFunctions.js
+++ src/allFunctions.js
@@ -7,6 +7,20 @@ const myFunctions = {
   reverseStr: (str) => {
     return str.split("").reverse().join("");
   },
+  calculator: {
+    add: (a, b) => {
+      return a + b;
+    },
+    sub: (a, b) => {
+      return a - b;
+    },
+    mul: (a, b) => {
+      return a * b;
+    },
+    div: (a, b) => {
+      return a / b;
+    },
+  },
 };
 
 export { myFunctions };
